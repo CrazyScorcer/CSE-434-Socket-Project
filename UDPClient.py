769c2e309178bc06a2d6a61d3c5805e930968846
CrazyScorcer <ericmorse121@gmail.com>
2022-09-24 13:24:06 -0700

    testing branch

--- UDPClient.py
+++ UDPClient.py
@@ -2,9 +2,11 @@ from socket import *
 serverIP = gethostbyname(gethostname()) #gethostbyname(gethostname()) used for localhost. For other uses put server IP
 serverPort = 28000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(),(serverIP, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
-    
+
+while(userInput != "exit"):
+    clientSocket.sendto(userInput.encode(),(serverIP, serverPort))
+    userInput = input("Type command: ")
+    clientSocket.sendto(userInput.encode(),(serverIP, serverPort))
+    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    print(modifiedMessage.decode())
 clientSocket.close()
