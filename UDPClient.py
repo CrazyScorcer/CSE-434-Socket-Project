d551a256c9d7ab1760f57fb212a103ec6f08b743
DHoang14 <dhoang9@asu.edu>
2022-09-24 17:27:08 -0700

    added follow function

--- UDPClient.py
+++ UDPClient.py
@@ -1,10 +1,31 @@
 from socket import *
-serverIP = gethostbyname(gethostname()) #gethostbyname(gethostname()) used for localhost. For other uses put server IP
+import pickle
+#class User():
+#	def _init_(self, handle, address):
+#		self.handle = ""
+#		self.address = ""
+class Follow:
+	def __init__(self, user, target):
+		self.user = user
+		self.target = target
+
+serverIP = gethostbyname(getfqdn()) #gethostbyname(gethostname()) used for localhost. For other uses put server IP
 serverPort = 28000
+
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(),(serverIP, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+
+command = ""
+while command != 'exit':
+    command = input('Enter command: ')
+    cmdList = command.split(" ")
+    if cmdList[0] == 'Follow':
+        msg = Follow(cmdList[1], cmdList[2])
+        message = pickle.dumps(msg)
+
+#print('before sending to server ', msg.user, ' ', msg.target)
+
+        clientSocket.sendto(message,(serverIP, serverPort))
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+#print(pickle.loads(modifiedMessage).user, ' ', pickle.loads(modifiedMessage).target)
     
 clientSocket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -1,12 +1,36 @@
 from http import server
 from socket import *
+import pickle
+class Follow:
+	def __init__(self, user, target):
+		self.user = user
+		self.target = target
+
+
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverIP = gethostbyname(gethostname()) #gethostbyname(gethostname()) used for localhost. For other uses put server IP
+serverIP = gethostbyname(getfqdn()) #gethostbyname(gethostname()) used for localhost. For other uses put server IP
 serverPort = 28000
 serverAddress = (serverIP,serverPort)
 serverSocket.bind(serverAddress)
+
+followerLists = []
+followingA = []
+followingB = []
+followingC = []
+followerLists.append(followingA)
+followerLists.append(followingB)
+followerLists.append(followingC)
+
+registered = ['A', 'B', 'C']
+
 print('The server is ready to receive')
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    modifiedMessage = pickle.loads(message)
+    print('before modification ', followerLists)
+    if type(modifiedMessage) is Follow :
+        index = registered.index(modifiedMessage.target)
+        followerLists[index].append(modifiedMessage.user)
+        followerLists[index].sort()
+    print('after receiving msg ', followerLists) 
+    serverSocket.sendto(pickle.dumps(modifiedMessage), clientAddress)
